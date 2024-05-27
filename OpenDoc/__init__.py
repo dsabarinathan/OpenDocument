10771cba6c7994f5f7e9af447fc538c538966d4b
Sabarinathan <40907627+dsabarinathan@users.noreply.github.com>
2024-05-27 21:18:47 +0900

    updated the code and preprocessing function

---
+++ OpenDoc/__init__.py
@@ -0,0 +1,6 @@
+# OpenDoc 🚀, AGPL-3.0 license
+
+__version__ = "0.0.1"
+
+# __init__.py
+from .preprocessing import sauvola_thresholding,remove_Lines
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 18:42:38 2024
+
+@author: SABARI
+"""
+
+from setuptools import setup , find_packages
+
+setup(
+      name = 'OpenDoc',
+      version = '0.1',
+      packages = find_packages(),
+      install_requires =[
+          # add depedencies here,
+          # e.g. 'numpy>=1.11.1'
+          'scikit-image==0.18.3',
+          'numpy==1.20.0',
+          'opencv-python==4.9.0.80'
+          ],
+      )
